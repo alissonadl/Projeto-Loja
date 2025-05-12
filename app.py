e8b6a9cbf9b2ab753a7c9d10ef42ee884e8bcfed
Alisson Araújo de Lima <160548927+alissonadl@users.noreply.github.com>
2025-05-12 14:40:11 +0000

    Criando rotas e CSS

--- app.py
+++ app.py
@@ -3,5 +3,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template ("index.html")
+def inicio():
+    return render_template("inicio.html")
+
+@app.route("/produtos")
+def produtos():
+    return render_template("produtos.html")
+
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
